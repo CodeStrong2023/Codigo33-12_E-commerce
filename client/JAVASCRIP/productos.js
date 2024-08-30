2b6f449c6e7371b29e755e4d72eca3e8d9b6057f
Gianella A. Achetoni <gianellaachetoni@gmail.com>
2024-08-30 12:02:49 -0300

    Update de organizcion de productos

---
+++ client/JAVASCRIP/productos.js
@@ -0,0 +1,56 @@
+const productos=[
+    {
+        id:111,
+        productName:'Adidas Gazelle',
+        price:480.000,
+        quanty:1,
+        imagen:'https://github.com/CodeStrong2023/Codigo33-12_E-commerce/blob/3a46b0420c2f2b94a8da763b34c94cf3a3b87a9a/client/Imagenes/adidasGazelle/Gazellenegra.png'
+    },
+    {
+        id:112,
+        productName:'Adidas Gazelle',
+        price:480.000,
+        quanty:1,
+        imagen:'https://github.com/CodeStrong2023/Codigo33-12_E-commerce/blob/3a46b0420c2f2b94a8da763b34c94cf3a3b87a9a/client/Imagenes/adidasGazelle/GazelleRoja.png'
+    },
+    {
+        id:113,
+        productName:'Adidas Gazelle',
+        price:480.000,
+        quanty:1,
+        imagen:'https://github.com/CodeStrong2023/Codigo33-12_E-commerce/blob/3a46b0420c2f2b94a8da763b34c94cf3a3b87a9a/client/Imagenes/adidasGazelle/GazelleVerde.png'
+    },
+    {
+        id:121,
+        productName:'Adidas La trainer',
+        price:480.000,
+        quanty:1,
+        imagen:'https://github.com/CodeStrong2023/Codigo33-12_E-commerce/blob/3a46b0420c2f2b94a8da763b34c94cf3a3b87a9a/client/Imagenes/AdidasLaTrainer/LatrainerNegroNa.png'
+    },
+    {
+        id:125,
+        productName:'Adidas La trainer',
+        price:480.000,
+        quanty:1,
+        imagen:'https://github.com/CodeStrong2023/Codigo33-12_E-commerce/blob/3a46b0420c2f2b94a8da763b34c94cf3a3b87a9a/client/Imagenes/AdidasLaTrainer/LatrainerAzul.png'
+    },
+    {
+        id:124,
+        productName:'Adidas La trainer',
+        price:480.000,
+        quanty:1,
+        imagen:'https://github.com/CodeStrong2023/Codigo33-12_E-commerce/blob/3a46b0420c2f2b94a8da763b34c94cf3a3b87a9a/client/Imagenes/AdidasLaTrainer/LatrainerGrisos.png'
+    },
+
+]
+
+
+
+
+// Adidas lleva el id = 1 que corresponde al primer numero del id, despues el modelo segun la carga se le pone otro id,
+// ese id corresponde al segundo numero y el color es el tercer numero del id por eso lleva 3 numeros :)
+// cada color debe llevar un numero ya asignado, es decir 1 = negro y asi con cada color 
+// Resumen Formacion del id :: Marca=1er numero; MOdelo:2do numero; Color=3er numero 
+// Si el id lleva mas de 3 numeros quiere decir que lleva mas de un color de referencia
+// Colores /1=negro/2=Rojo/3=Verde/4=Gris/5=Azul/6=blanco/7=Beige/8=Amarillo
+// Marcas /Adidas=1/Nike=2/=3/=4/=5/=6/=7/
