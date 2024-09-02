fd4b214f88bbcd85b59934378712eaad7546a328
Gianella A. Achetoni <gianellaachetoni@gmail.com>
2024-09-02 11:12:51 -0300

    update cambios y mejooras

--- client/JAVASCRIP/app.js
+++ client/JAVASCRIP/app.js
@@ -3,26 +3,29 @@ const cart = [];
 
 productos.forEach((producto) => {
     const content = document.createElement("div");
+    content.classList.add("product"); // Agregar clase
+
     content.innerHTML = `
-    <img src="${producto.imagen}">
-    <h3>${producto.productName}</h3>
-    <p>${producto.price}</p>
+        <img src="${producto.imagen}" alt="${producto.productName}">
+        <h3>${producto.productName}</h3>
+        <p>${producto.price}</p>
     `;
     shopContent.append(content);
 
     const buyButton = document.createElement("button");
     buyButton.innerText = "Comprar";
+    buyButton.classList.add("buy-button"); // Agregar clase
 
     content.append(buyButton);
 
-    buyButton.addEventListener("click",()=>{
+    buyButton.addEventListener("click", () => {
         cart.push({
-            id:producto.id,
-            productName:producto.productName,
-            price:producto.price,
-            quanty:producto.quanty,
-            imagen:producto.imagen
-        })
-        console.log(cart)
-    } )
-})
+            id: producto.id,
+            productName: producto.productName,
+            price: producto.price,
+            quanty: producto.quanty,
+            imagen: producto.imagen
+        });
+        console.log(cart);
+    });
+});
