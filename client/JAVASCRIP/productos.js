d830de08e96f7830c03e4c6edd1d5e72bd6160c6
Gianella A. Achetoni <gianellaachetoni@gmail.com>
2024-09-05 18:43:27 -0300

    Update modificación

--- client/JAVASCRIP/productos.js
+++ client/JAVASCRIP/productos.js
@@ -47,7 +47,9 @@ const productos=[
 // Adidas lleva el id = 1 que corresponde al primer numero del id, despues el modelo segun la carga se le pone otro id,
 // ese id corresponde al segundo numero y el color es el tercer numero del id por eso lleva 3 numeros :)
 // cada color debe llevar un numero ya asignado, es decir 1 = negro y asi con cada color 
+//
 // Resumen Formacion del id :: Marca=1er numero; MOdelo:2do numero; Color=3er numero 
+//
 // Si el id lleva mas de 3 numeros quiere decir que lleva mas de un color de referencia
 // Colores /1=negro/2=Rojo/3=Verde/4=Gris/5=Azul/6=blanco/7=Beige/8=Amarillo
 // Marcas /Adidas=1/Nike=2/=3/=4/=5/=6/=7/
